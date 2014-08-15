cf3e8aadeaded8040051c445185300a2a8f2d25a
Carsten Blüm <carsten@bluem.net>
2014-08-15 12:59:06 +0200

    Initial commit, as of Pashua 0.9.5.1

---
+++ example.tcl
@@ -0,0 +1,106 @@
+#!/usr/bin/tclsh
+
+#
+# USAGE INFORMATION:
+# As you can see this text, you obviously have opened the file in a text editor.
+# 
+# If you would like to *run* this example rather than *read* it, you
+# should open Terminal.app, drag this document's icon onto the terminal
+# window, bring Terminal.app to the foreground (if necessary) and hit return.
+# 
+if {[file exists "[file dirname [info script]]/pashua_run.tcl"]} {
+	source "[file dirname [info script]]/pashua_run.tcl"
+} else {
+	source pashua_run.tcl
+}
+
+set config "# Set transparency: 0 is transparent, 1 is opaque
+*.transparency=0.95
+
+# Set window title
+*.title = Introducing Pashua
+
+# Introductory text
+tb.type = text
+tb.default = Pashua is an application for generating dialog windows from programming languages which lack support for creating native GUIs on Mac OS X. Any information you enter in this example window will be returned to the calling script when you hit \"OK\"; if you decide to click \"Cancel\" or press \"Esc\" instead, no values will be returned. This window demonstrates nine of the GUI widgets that are currently available. You can find a full list of all GUI elements and their corresponding attributes in the documentation that is included with Pashua.
+tb.height = 276
+tb.width = 310
+tb.x = 340
+tb.y = 44
+
+
+# Add a text field
+tx.type = textfield
+tx.label = Example textfield
+tx.default = Textfield content
+tx.width = 310
+
+# Add a filesystem browser
+ob.type = openbrowser
+ob.label = Example filesystem browser (textfield + open panel)
+ob.width=310
+ob.tooltip = Blabla filesystem browser
+
+# Define radiobuttons
+rb.type = radiobutton
+rb.label = Example radiobuttons
+rb.option = Radiobutton item #1
+rb.option = Radiobutton item #2
+rb.option = Radiobutton item #3
+rb.option = Radiobutton item #4
+rb.default = Radiobutton item #2
+
+# Add a popup menu
+pop.type = popup
+pop.label = Example popup menu
+pop.width = 310
+pop.option = Popup menu item #1
+pop.option = Popup menu item #2
+pop.default = Popup menu item #2
+
+# Add a checkbox
+chk1.type = checkbox
+chk1.label = Pashua offers checkboxes, too
+chk1.rely = -18
+chk1.default = 1
+
+# Add another one
+chk2.type = checkbox
+chk2.label = But this one is disabled
+chk2.disabled = 1
+
+# Add a cancel button with default label
+cb.type=cancelbutton
+
+"
+
+# Set the images' paths relative to this file's path / 
+# skip images if they can not be found in this file's path
+set icon "[file dirname [info script]]/.icon.png"
+set bgimg "[file dirname [info script]]/.demo.png"
+
+if {[file exists $icon]} then {
+	# Display Pashua's icon
+	append config "img.type = image\nimg.x = 530\nimg.y = 255\nimg.path = $icon\n";
+}
+
+if {[file exists $bgimg]} then {
+	# Display background image
+	append config "bg.type = image\nbg.x = 30\nbg.y = 2\nbg.path = $bgimg\n";
+}
+
+set res [pashua_run $config utf8]
+
+array set x $res
+if {$x(cb)==1} then {
+	puts "Looks like the dialog was cancelled"
+} else {
+	puts "Pashua returned the following values:"
+	puts " cb   = $x(cb)"
+	puts " chk1 = $x(chk1)"
+	puts " chk2 = $x(chk2)"
+	puts " ob   = $x(ob)"
+	puts " pop  = $x(pop)"
+	puts " rb   = $x(rb)"
+	puts " tx   = $x(tx)"
+}
---
+++ pashua_run.tcl
@@ -0,0 +1,65 @@
+# Glue code to use Pashua from Tcl. Pashua is a Mac OS X
+# application for generating native Aqua GUIs from scripting
+# languages: http://www.bluem.net/downloads/pashua_en/
+# This Tcl code was contributed by Bruce M. Axtens.
+# Additional improvements / changes by Carsten Bluem
+proc pashua_run {script {encoding ""} {path ""}} {
+
+	upvar #0 argv0 appName
+	upvar #0 env   envir
+
+	set tempFile [exec /usr/bin/mktemp /tmp/Pashua_XXXXXXXX]
+	set handle [open $tempFile w]
+	puts $handle $script
+	close $handle
+	
+	# Search for Pashua binary
+	set bundlePath "Pashua.app/Contents/MacOS/Pashua"
+	set theHome $envir(HOME)
+	set theList [list "$appName/Pashua" \
+		"$appName/$bundlePath" \
+		"/$bundlePath" \
+		"[file dirname [info script]]/Pashua" \
+		"[file dirname [info script]]/$bundlePath" \
+		"./$bundlePath" \
+		"/Applications/$bundlePath" \
+		"$theHome/Applications/$bundlePath" ]
+
+	if {$path != ""} {
+		# Prepend $path to the list of search paths
+		set theList [linsert $theList 0 "$path/$bundlePath"]
+	}
+
+	if {$encoding != ""} {
+		# Set encoding argument
+		set encoding "-e $encoding"
+	}
+
+	set theBinary ""
+	foreach possib $theList {
+		if {[file exists $possib]} then {
+			if {[file executable $possib]} then {
+				set theBinary $possib
+				break
+			}
+		}
+	}
+
+	if {$theBinary != ""} {
+		set handle [open "|\"$theBinary\" $encoding $tempFile" r]
+		while {[eof $handle]==0} {
+			gets $handle theLine
+			set equals [string first {=} $theLine]
+			if {$equals > -1} {
+				array set results \
+				[list [string range $theLine 0 [expr $equals - 1]] \
+				[string range $theLine [expr $equals + 1] end]]
+			}
+		}
+		return [array get results]
+		
+	} else {
+		return ""
+	}
+
+}
